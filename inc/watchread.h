0ce05dc87e9644e1d2715688b31ccdd6d8b553a2
Carups <Nicorab@mail.ru>
2017-11-03 00:44:28 +0300

    Add files via upload

---
+++ inc/watchread.h
@@ -0,0 +1,18 @@
+#ifndef __watchread_H__
+#define __watchread_H__
+
+#include <watch_app.h>
+#include <watch_app_efl.h>
+#include <Elementary.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "watchread"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.watchread"
+#endif
+
+#endif /* __watchread_H__ */
